973aa564194ae477152f1b22080c278b5652124b
Cindy Leschaud <cindy.leschaud@gmail.com>
2014-12-30 17:35:07 +0100

    styles for groups

--- app/views/site/users/index.blade.php
+++ app/views/site/users/index.blade.php
@@ -7,15 +7,18 @@
         <div class="container">
 
             <div class="row">
-                <div class="col-md-6 text-center partage-user">
+                <div class="col-md-5 text-center partage-user">
                     <div class="userPicto userPicto-host">
                         <div class="thumb rotate">
-                            <img src="{{ asset('users/user_1.jpg') }}" alt="" />
+                            <img class="riiinglinkIcon" src="{{ asset('users/user_1.jpg') }}" alt="" />
                         </div>
                         <h4 class="factTitle">Cindy Leschaud</h4>
                     </div><!-- end of fact -->
                 </div><!-- end of facts wrapper -->
-                <div class="col-md-6 text-center partage-user">
+                <div class="col-md-2 text-center riiinglinkIcon">
+                    <img src="{{ asset('images/riiinglink.svg') }}" alt="" />
+                </div><!-- end of facts wrapper -->
+                <div class="col-md-5 text-center partage-user">
                     <div class="userPicto userPicto-invited">
                         <div class="thumb rotate">
                             <img src="{{ asset('users/user_2.jpg') }}" alt="" />
@@ -35,15 +38,21 @@
                         <div class="partage-host">
 
                             @if(!empty($grouped))
+                                @foreach($grouped as $index => $group)
 
-                                @foreach($grouped as $group)
-                                    <ul class="partage-group">
-                                    @foreach($group as $label)
-                                        <li <?php echo (in_array($label->id, $metas) ? 'class="used"' : '' ); ?>><span>{{ $label->type->titre }}</span><strong>{{ $label->label }}</strong></li>
-                                    @endforeach
-                                    </ul>
+                                    <div class="partage-icon">
+                                        <div class="groupe-icons">
+                                            <div class="groupe-icon-info groupe-icon-{{ $index }}"></div>
+                                        </div>
+
+                                        <ul class="partage-group">
+                                            @foreach($group as $label)
+                                                <li <?php echo (in_array($label->id, $metas) ? 'class="used"' : '' ); ?>><span>{{ $label->type->titre }}</span><strong>{{ $label->label }}</strong></li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+
                                 @endforeach
-
                             @endif
                         </div>
                     </div>
@@ -51,12 +60,20 @@
                         <div class="partage-invited">
                             @if(!empty($riiinglink2))
 
-                                @foreach($riiinglink2 as $linkgroupe)
-                                    <ul class="partage-group">
-                                        @foreach($linkgroupe as $label2)
-                                            <li class="used"><span>{{ $types[$label2->type_id] }}</span><strong>{{ $label2->label }}</strong></li>
-                                        @endforeach
-                                    </ul>
+                                @foreach($riiinglink2 as $index => $linkgroupe)
+
+                                    <div class="partage-icon">
+                                        <div class="groupe-icons">
+                                            <div class="groupe-icon-info groupe-icon-{{ $index }}"></div>
+                                        </div>
+
+                                        <ul class="partage-group">
+                                            @foreach($linkgroupe as $label2)
+                                                <li class="used"><span>{{ $types[$label2->type_id] }}</span><strong>{{ $label2->label }}</strong></li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+
                                 @endforeach
 
                             @endif
